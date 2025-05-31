0dd445e9eb23dfb570b3f5bade0c91fe34203b8c
Andrey Sitnik <andrey@sitnik.ru>
2025-05-31 23:10:50 +0900

    Update API and syntax

--- extension.js
+++ extension.js
@@ -3,17 +3,17 @@ import { panel } from 'resource:///org/gnome/shell/ui/main.js'
 let keyboard = panel.statusArea.keyboard
 
 export default class HideKeyboardLayoutExtension {
-  watching = null
+  #watching = null
 
   disable() {
-    if (this.watching) keyboard.actor.disconnect(this.watching)
+    if (this.#watching) keyboard.disconnect(this.#watching)
     keyboard.show()
   }
 
   enable() {
     keyboard.hide()
-    this.watching = keyboard.actor.connect('notify::visible', actor => {
-      actor.hide()
+    this.#watching = keyboard.connect('notify::visible', actor => {
+      keyboard.hide()
     })
   }
 }
